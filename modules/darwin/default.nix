22c33d9027255c5f2c5be97e2acc96bb1efa6f26
Kai Ian Hendry <hendry@iki.fi>
2023-06-20 17:35:53 +0100

    try out stuff from https://github.com/MatthiasBenaets/nixos-config/tree/2dce40f16f2f0778511ad12cd6832d05772b2d45

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -25,30 +25,39 @@
   fonts.fontDir.enable = true;
   fonts.fonts = [ (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; }) ];
   services.nix-daemon.enable = true;
-  system.defaults = {
-    finder.AppleShowAllExtensions = true;
-    finder._FXShowPosixPathInTitle = true;
-    dock.autohide = true;
+  system = {
+    defaults = {
+      finder.AppleShowAllExtensions = true;
+      finder._FXShowPosixPathInTitle = true;
+      dock.autohide = true;
 
-    NSGlobalDomain = {
-      "com.apple.sound.beep.feedback" = 0;
-      "com.apple.sound.beep.volume" = 0.0;
-      # allow key repeat
-      ApplePressAndHoldEnabled = false;
-      # delay before repeating keystrokes
-      InitialKeyRepeat = 15; # normal minimum is 15 (225 ms)
-      # delay between repeated keystrokes upon holding a key
-      KeyRepeat = 2; # normal minimum is 2 (30 ms)
-      AppleShowAllExtensions = true;
-      AppleShowScrollBars = "Automatic";
+      NSGlobalDomain = {
+        "com.apple.sound.beep.feedback" = 0;
+        "com.apple.sound.beep.volume" = 0.0;
+        # allow key repeat
+        ApplePressAndHoldEnabled = false;
+        # delay before repeating keystrokes
+        InitialKeyRepeat = 15; # normal minimum is 15 (225 ms)
+        # delay between repeated keystrokes upon holding a key
+        KeyRepeat = 2; # normal minimum is 2 (30 ms)
+        AppleShowAllExtensions = true;
+        AppleShowScrollBars = "Automatic";
+      };
     };
+    # backwards compat; don't change
+    stateVersion = 4;
+
+    activationScripts.postActivation.text = ''sudo chsh -s ${pkgs.bashInteractive}/bin/bash''; # Since it's not possible to declare default shell, run this command after build
+
   };
-  # backwards compat; don't change
-  system.stateVersion = 4;
+
   homebrew = {
     enable = true;
     caskArgs.no_quarantine = true;
     # use home brew to install packages for spotlight to work
+    brews = [
+      "aws-sam-cli"
+    ];
     casks = [ "maccy" "flameshot" "visual-studio-code" "alacritty" ];
   };
 }
