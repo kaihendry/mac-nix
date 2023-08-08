a26391fea7a766faf3e482a754cb1d07c3ffe62d
Kai Hendry <hendry@iki.fi>
2023-08-08 15:24:40 +0100

    git signs setup

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,6 +1,5 @@
 # https://nix-community.github.io/home-manager/options.html
 { pkgs, ... }: {
-  # Don't change this when you change package input. Leave it alone.
   home.stateVersion = "22.11";
 
   home.packages = with pkgs; [
@@ -54,6 +53,8 @@
     defaultEditor = true;
 
     extraConfig = ''
+      lua require('gitsigns').setup()
+
       set list listchars=nbsp:¬,tab:»·,trail:·,extends:>
       set shiftwidth=4
       set softtabstop=4
@@ -72,33 +73,16 @@
       :au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
     '';
 
-    plugins = [
-      {
-        plugin = pkgs.vimPlugins.vim-nix;
-      }
-      {
-        plugin = pkgs.vimPlugins.vim-fugitive;
-      }
-      {
-        plugin = pkgs.vimPlugins.vim-rhubarb;
-      }
-      {
-        plugin = pkgs.vimPlugins.copilot-vim;
-      }
-      {
-        plugin = pkgs.vimPlugins.vim-commentary;
-      }
-      {
-        plugin = pkgs.vimPlugins.gitsigns-nvim;
-        type = "lua";
-        config = ''
-          require('gitsigns').setup({})
-        '';
-      }
-      {
-        plugin = pkgs.vimPlugins.dracula-vim; # dark colorscheme
-      }
+    plugins = with pkgs.vimPlugins; [
+      vim-commentary
+      vim-fugitive
+      vim-nix
+      vim-rhubarb
+      copilot-vim
+      dracula-vim
+      gitsigns-nvim
     ];
+
   };
 
   programs.fzf.enable = true;
